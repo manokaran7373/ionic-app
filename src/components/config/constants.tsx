e5bef3dacc382ee073944dbf9f332c0ccdc73ef7
manokaran7373 <manokarank1916@gmail.com>
2025-06-27 12:38:17 +0530

    added hosted base url

--- src/components/config/constants.tsx
+++ src/components/config/constants.tsx
@@ -1,4 +1,4 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000';
+export const API_BASE_URL = 'https://geo-fencing-backendnew.onrender.com';
 
 export const API_ENDPOINTS = {
   LOGIN: 'api/login/',
